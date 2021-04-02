d3160c48492bb04cbfe307bedaff114517834450
andeen171 <zeander21@outlook.com.br>
2021-04-02 18:24:34 -0300

    Código Comentado

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from UiCalculadora import Ui_MainWindow
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets # PyQt5 Nescessario pra rodar
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -7,10 +7,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
         global lista
-        lista = []
+        lista = [] # lista nescessaria para montar a expressao
         class status: status = True
 
-        def calc(arg):
+        def calc(arg): # Check dos botões Ce e Off
             if arg == 'Off':
                 self.visor.display('')
                 lista.clear()
@@ -18,7 +18,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.visor.display('0')
                 lista.clear()
 
-            elif arg in '1234567890.':
+            elif arg in '1234567890.': # Quando precionar um numero
                 valor = str(self.visor.intValue())
                 if len(lista) and lista[-1] == '.':
                     valor += '.'
@@ -28,22 +28,22 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                     self.visor.display(str(arg))
                 lista.append(arg)
                 status.status = True
-            elif arg in '+-*/' and (all(map(lambda x: lista.count(x) <= 1, ['+', '-', '*', '/'])) and len(lista) and '+-'):
+            elif arg in '+-*/' and (all(map(lambda x: lista.count(x) <= 1, ['+', '-', '*', '/'])) and len(lista) and '+-'): # quando for um operador
                 status.status = False
                 if len(lista) and '+-*/' in lista[-1]:
                     del lista[-1]
                 lista.append(arg)
-            else:
+            else: # do contrario tenta exibir o resultado
                 if len(lista):
-                    print(lista)
+                    print(lista) # exibe a lista na janela do console para averiguação
                     status.status = False
-                    self.visor.display(str(eval(''.join(lista).strip('+-*/='))))
-                    lista.clear()
+                    self.visor.display(str(eval(''.join(lista).strip('+-*/=')))) # exibe o resultado da expressao feito com eval()
+                    lista.clear() 
                     resultado = str(self.visor.value())
                     if resultado.endswith('.0'):
                         resultado = resultado.rsplit('.0', 1)[0]
                     lista.append(resultado)
-                    if arg != '=':
+                    if arg != '=': # se a ultima operação não for "=" o resultado e colocado na lista para proximos calculos
                         lista.append(arg)
 
         self.botao0_2.clicked.connect(lambda: calc(self.botao0_2.text()))
